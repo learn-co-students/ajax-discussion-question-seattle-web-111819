bfe5cab4751bcb912872e32a222f80ce3a2f44df
bahayg <bahayg@gmail.com>
2020-01-08 10:29:01 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,29 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  const addButton = document.querySelector("button")
+  addButton.addEventListener("click", function () {
+    getData()
+  })
+  
 });
+
+
+function getData() {
+fetch ("https://randomuser.me/api/")
+.then (res => res.json())
+.then (data => addPerson(data))
+}
+
+function addPerson(data) {
+
+  const h3 = document.getElementById("fullname")
+  h3.innerText = data.results[0].name.first;
+  
+  const h4 = document.getElementById("email")
+  h4.textContent = data.results[0].email;
+
+  const img = document.getElementById("profile_picture")
+  img.src = data.results[0].picture;
+
+}
